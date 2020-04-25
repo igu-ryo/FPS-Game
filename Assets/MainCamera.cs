4442976a94d4f8510851b64f4fd2820ddcb20987
igu-ryo <49966687+igu-ryo@users.noreply.github.com>
2020-04-25 14:56:33 +0900

    アセットの追加

---
+++ Assets/MainCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    GameObject targetObj;
+    Vector3 targetPos;
+    public float cameraSpeed;
+
+	// Use this for initialization
+	void Start () {
+        targetObj = GameObject.Find("Player");
+        targetPos = targetObj.transform.position;
+
+        // プレイヤーと一定距離離れたところにカメラ位置を初期化
+        Vector3 cameraDistance = new Vector3(0, 1, 0);
+        transform.position = targetPos + cameraDistance;
+
+        // カメラの向きをプレイヤー方向に向ける
+        //transform.rotation = Quaternion.LookRotation(targetPos - transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // targetの移動量分、自分（カメラ）も移動する
+        transform.position += targetObj.transform.position - targetPos;
+        targetPos = targetObj.transform.position;
+
+        float horizontal = 0;
+        float vertical = 0;
+        if (Input.GetKey(KeyCode.L)) horizontal = 1;
+        if (Input.GetKey(KeyCode.J)) horizontal = -1;
+        if (Input.GetKey(KeyCode.I)) vertical = 1;
+        if (Input.GetKey(KeyCode.K)) vertical = -1;
+        // targetの位置のY軸を中心に、回転（公転）する
+        transform.RotateAround(targetPos, Vector3.up, horizontal * cameraSpeed);
+        // カメラの垂直移動
+        transform.RotateAround(targetPos, transform.right, vertical * cameraSpeed);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Rigidbody rb;
+    //プレイヤーの移動速度
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        this.rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = 0;
+        float vertical = 0;
+        if (Input.GetKey(KeyCode.F)) horizontal = 1;
+        if (Input.GetKey(KeyCode.S)) horizontal = -1;
+        if (Input.GetKey(KeyCode.E)) vertical = 1;
+        if (Input.GetKey(KeyCode.D)) vertical = -1;
+        //カメラの向きのy成分を除いた単位ベクトル
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
+        //カメラの向きと方向キーから移動方向の単位ベクトルを決める
+        Vector3 moveForward = (cameraForward * vertical + Camera.main.transform.right * horizontal).normalized;
+        //移動方向にスピードを掛け、y方向の速度は触らない
+        rb.velocity = moveForward * moveSpeed + new Vector3(0, rb.velocity.y, 0);
+        //プレイヤーの向きを進行方向に
+        if(moveForward != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveForward);
+        }
+    }
+}
